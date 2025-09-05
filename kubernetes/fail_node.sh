d4fc77db7f56dea90e62706b43ef22ce0f2d8157
ulchatur <34676077+ulchatur@users.noreply.github.com>
2025-09-05 15:20:46 +0530

    Delete kubernetes directory

--- kubernetes/fail_node.sh
+++
@@ -1,2 +0,0 @@
-# ssh into the Kubernetes node based on the type of kubernetes cluster being used and run below command to fail
-sudo systemctl stop kubelet
--- kubernetes/heal_node.sh
+++
@@ -1,4 +0,0 @@
-#update the node name based on your kubernetes cluster
-
-kubectl cordon node1
-kubectl delete node node1
